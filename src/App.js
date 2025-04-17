d7b3b8807607e4cba781576188c64ac044f007c8
Time_Traveller <152459170+terepapakamaal@users.noreply.github.com>
2025-04-17 21:56:42 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { useTheme, useMediaQuery } from '@mui/material';
+import Navbar from './components/Navbar';
+import BottomNav from './components/BottomNav';
 import HomePage from './components/HomePage';
 import UrlSafetyChecker from './components/UrlSafetyChecker';
 import SmsScamChecker from './components/SmsScamChecker';
@@ -10,8 +13,12 @@ import EducationPage from './components/EducationPage';
 import EmailBreachRedirect from './components/EmailBreachRedirect';
 
 function App() {
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
+
   return (
     <Router>
+      {isMobile ? <BottomNav /> : <Navbar />}
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/url-checker" element={<UrlSafetyChecker />} />
